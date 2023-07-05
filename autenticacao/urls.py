fb33d23372f8ed2db94ae28461463a7c89ce539e
AndreConforti <andre.r.m.conforti@gmail.com>
2023-07-05 13:30:41 -0300

    primeiro commmit. Adicionado o arquivo base.html

---
+++ autenticacao/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'autenticacao'
+
+urlpatterns = [
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logar/', views.logar, name='logar'),
+]
