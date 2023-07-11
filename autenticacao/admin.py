9f8d200c00c18b3c2bcde8da5c00cb36f45b5703
AndreConforti <andre.r.m.conforti@gmail.com>
2023-07-11 13:21:16 -0300

    1:14 de aula

--- autenticacao/admin.py
+++ autenticacao/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
 
 # Register your models here.
+# andre@gmail.com   /   Andre1
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.contrib.auth.models import User
 
 
 def cadastro(request):
@@ -11,7 +13,20 @@ def cadastro(request):
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
 
-        return HttpResponse(confirmar_senha)
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        
+    try:
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha,
+            is_active=False   # não é para que o usuário esteja ativo assim que ele for criado, será enviado por email uma confirmação
+        )
+        user.save()
+        return redirect('/auth/logar')
+    except:
+        return redirect('/auth/cadastro')
 
 
 def logar(request):
