1d6b6fd2a2f574ce7bb7e8bf962f1ec1e674a95e
AndreConforti <andre.r.m.conforti@gmail.com>
2023-07-13 13:16:39 -0300

    chegamos a 1:30 da primeira aula

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .utils import password_is_valid
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from .utils import password_is_valid
 
 
 def cadastro(request):
@@ -24,11 +26,27 @@ def cadastro(request):
             is_active=False   # não é para que o usuário esteja ativo assim que ele for criado, será enviado por email uma confirmação
         )
         user.save()
+        messages.add_message(request, 'Usuário cadastrado com sucesso!')
         return redirect('/auth/logar')
+    
     except:
+        messages.add_message(request, constants.ERROR, 'Erro interno do sistema.')
         return redirect('/auth/cadastro')
 
 
 def logar(request):
-    return HttpResponse('Página de login')
+    if request.method == "GET":
+        return render(request, 'logar.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+
+        usuario = auth.authenticate(username=username, password=senha)
+
+        if not usuario:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect('/auth/logar')
+        else:
+            auth.login(request, usuario)
+            return redirect('/')
 
